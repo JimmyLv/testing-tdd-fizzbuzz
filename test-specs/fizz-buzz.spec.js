a248902128a1219c15c1bdd2c5fe5a50eae21657
JingLv-ND <55941758+JimmyLv-ND@users.noreply.github.com>
2019-12-05 22:29:00 +0800

    实现 FizzBuzz 功能

--- test-specs/fizz-buzz.spec.js
+++ test-specs/fizz-buzz.spec.js
@@ -1,7 +1,7 @@
 import * as module from "../src/fizz-buzz";
 
 // jest.mock('./fizz-buzz');
-jest.spyOn(module, 'fizzBuzz');
+jest.spyOn(module, "fizzBuzz");
 const { fizzBuzz } = module;
 
 test("should return number string when given normal number", () => {
@@ -11,6 +11,33 @@ test("should return number string when given normal number", () => {
   const result = fizzBuzz(number);
   // then
   expect(result).toBe("2");
+});
+
+test("should return Fizz string when given number divisble by 3", () => {
+  // given
+  const number = 3;
+  // when
+  const result = fizzBuzz(number);
+  // then
+  expect(result).toBe("Fizz");
+});
+
+test("should return Buzz string when given number divisble by 5", () => {
+  // given
+  const number = 5;
+  // when
+  const result = fizzBuzz(number);
+  // then
+  expect(result).toBe("Buzz");
+});
+
+test("should return FizzBuzz string when given number divisble by 3 and 5", () => {
+  // given
+  const number = 15;
+  // when
+  const result = fizzBuzz(number);
+  // then
+  expect(result).toBe("FizzBuzz");
 });
 
 test("should verify tests code should be verified", () => {
@@ -24,7 +51,7 @@ test("should verify tests code should be verified", () => {
   expect(fizzBuzz).toHaveBeenCalledWith(15);
 
   // 异常情况
-  expect(fizzBuzz).toHaveBeenCalledWith('');
+  expect(fizzBuzz).toHaveBeenCalledWith("");
   expect(fizzBuzz).toHaveBeenCalledWith(101);
 
   expect(fizzBuzz).toBeCalledTimes(6); // 6 个用例
