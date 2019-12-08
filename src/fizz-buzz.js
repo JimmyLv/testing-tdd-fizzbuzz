4e98f9feae836c02840c86883c539d7626dbbe6c
Songboyuan <song.dynasty@gmail.com>
2019-12-08 22:19:45 +0800

    Tim Song TDD fizzbuzz
    Tim Song TDD fizzbuzz

--- src/fizz-buzz.js
+++ src/fizz-buzz.js
@@ -1,3 +1,14 @@
 export function fizzBuzz(number) {
-  return '2';
+  if (number <= 0 || number >= 100) {
+    throw new Error('Bad input.');
+  }
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (number % 3 === 0) {
+    return 'fizz';
+  } else if (number % 5 === 0) {
+    return 'buzz';
+  }
+
+  return number.toString();
 }
