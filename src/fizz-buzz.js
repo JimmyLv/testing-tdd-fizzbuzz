26a52d68101a3afc276d7e43e52c0133ff653d97
cuixiaorui <cui_xiaorui@126.com>
2019-12-12 16:07:34 +0800

    add test

--- src/fizz-buzz.js
+++ src/fizz-buzz.js
@@ -1,3 +1,23 @@
+const isAliquot3 = isAliquot(3);
+const isAliquot5 = isAliquot(5);
+
 export function fizzBuzz(number) {
-  return '2';
+  if(isAliquot3(number) && isAliquot5(number)){
+    return 'fizzBuzz'
+  }
+  if (isAliquot3(number)) {
+    return 'fizz';
+  }
+
+  if (isAliquot5(number)) {
+    return 'buzz';
+  }
+
+  return number;
+}
+
+function isAliquot(dividend){
+  return (number)=>{
+    return number % dividend === 0; 
+  }
 }
--- src/fizz-buzz.test.js
+++ src/fizz-buzz.test.js
@@ -1,10 +1,52 @@
 import { fizzBuzz } from './fizz-buzz';
 
-test('should return number string when given normal number', () => {
-  // given
-  const number = 2;
+function verifyHandler(number, expectResult) {
+  // give 
+  // number
+
   // when
   const result = fizzBuzz(number);
+
   // then
-  expect(result).toBe('2');
+  expect(result).toBe(expectResult);
+}
+
+describe('3 的倍数输出 fizz', () => {
+  it('输入 3 输出 fizz', () => {
+    verifyHandler(3, 'fizz');
+  });
+
+  it('输入 6 输出 fizz', () => {
+    verifyHandler(6, 'fizz');
+  });
+});
+
+describe('5 的倍数输出 buzz', () => {
+  it('输入 5 输出 buzz', () => {
+    verifyHandler(5, 'buzz');
+  });
+
+  it('输入 10 输出 fizz', () => {
+    verifyHandler(10, 'buzz');
+  });
+});
+
+describe('输入 3 和 5 的共同倍数输出 fizzBuzz', () => {
+  it('输入 15 输出 fizzBuzz', () => {
+    verifyHandler(15, 'fizzBuzz');
+  });
+
+  it('输入 30 输出 fizzBuzz', () => {
+    verifyHandler(30, 'fizzBuzz');
+  });
+});
+
+describe('不能被 3 整除, 也不能被 5 整除，输出 输入的数字', () => {
+  it('输入 22 ，应该也输出 22 ', () => {
+    verifyHandler(22, 22);
+  });
+
+  it('输入 2 ，应该也输出 2 ', () => {
+    verifyHandler(2, 2);
+  });
 });
